da347c089a64aab9df8f6a0db642c851ebc509b8
IlyaSedov <55514389+IlyaSedov@users.noreply.github.com>
2019-10-20 12:19:09 +0300

    Add files via upload

---
+++ scr/toDoList.js
@@ -0,0 +1,47 @@
+let list = document.querySelector('ul'),
+    todos;
+if(localStorage.getItem('todos')){
+  list.innerHTML=localStorage.getItem('todos');
+}
+const toLocal = () =>{
+  todos=list.innerHTML;
+  localStorage.setItem('todos',todos);
+}
+
+list.addEventListener('click', (ev) => {
+    if(ev.target.tagName === "LI") {
+       ev.target.classList.toggle('checked');
+       toLocal();
+    } 
+    if(ev.target.tagName === "SPAN") {
+      let div = ev.target.parentNode;
+       div.remove();
+       toLocal();
+    }
+    
+}, false);
+
+const newEl = () => {
+  let li = document.createElement('li'),
+     inputValue = document.getElementById('toDoEl').value;
+     if(inputValue == "") {
+      inputValue = "Пусто";
+    } 
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    
+       document.getElementById('list').appendChild(li);
+    
+    document.getElementById('toDoEl').value = "";
+    let span = document.createElement('SPAN'),
+     txt = document.createTextNode("X");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+    toLocal();
+}
+document.addEventListener('keydown', function(event) {
+  if (event.code == 'Enter') {
+    newEl();
+  }
+});
